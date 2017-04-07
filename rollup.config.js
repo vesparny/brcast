832c47ccd1d5b29fa7d0a1673aeeafae6c086840
Alessandro Arnodo <alessandro@arnodo.net>
2017-04-07 14:11:23 +0200

    remove unused dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 import buble from 'rollup-plugin-buble'
-import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   useStrict: false,
   plugins: [
-    buble(),
-    resolve({jsnext: true, main: true})
+    buble()
   ]
 }
