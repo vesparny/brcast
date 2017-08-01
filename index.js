1cc2c7d7232fba6cac89ca0d213c28611ad6ad8f
Alessandro Arnodo <alessandro@arnodo.net>
2017-08-01 19:01:39 +0200

    chore: remove delete listeners[id] in favour of listeners[id] = undefined (perf improvement)

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ export default function createBroadcast (initialState) {
 
   // remove subscription by removing the listener function
   function unsubscribe (id) {
-    if (listeners[id]) delete listeners[id]
+    if (listeners[id]) listeners[id] = undefined
   }
 
   return { getState, setState, subscribe, unsubscribe }
