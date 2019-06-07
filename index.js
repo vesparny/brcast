abf1f1de5ddf6c164f796cc543a14389009b7a80
Elliott Sprehn <esprehn@gmail.com>
2019-06-07 01:41:55 -0700

    Fix memory leak in unsubscribe
    Assigning undefined doesn't remove the key from the key set for listeners
    which means it was continuously growing. This also means setState is
    getting increasingly slower as it needs to skip over the now deleted keys
    on every setState. Instead we should use delete to remove the key.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ export default function createBroadcast (initialState) {
 
   // remove subscription by removing the listener function
   function unsubscribe (id) {
-    listeners[id] = undefined
+    delete listeners[id]
   }
 
   return { getState, setState, subscribe, unsubscribe }
